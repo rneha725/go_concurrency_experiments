ef3496f3fb390fd320a6615f39d86a2bfcdc5626
rneha725 <rneha725@gmail.com>
2024-06-04 12:41:47 +0530

    Add fanIn

---
+++ README.md
@@ -0,0 +1,6 @@
+Notes:
+- the sending and receiving on channel are sync functions, goroutine wait to send and receive.
+  - to solve this issue, we also have buffering channels [not covered]
+- 
+- [x] Fan-in multiplexing
+- 
---
+++ fan-in.go
@@ -0,0 +1,67 @@
+package main
+
+/*
+Fan-in:
+channel1----▼
+			|-----> channel -->
+channel2----▲
+
+Why do we need fan-in?
+Following simple reading from two channel will create two blocking calls for the receiver function, it will look
+something like this:
+
+f() {
+	forLoop()
+		print(from channel1) //blocking
+		print(from channel2) //blocking
+}
+
+We might not want to wait for the receiver on these channels, and want to process as soon as any of these channels get a
+value. This can be done using a third channel. What we can do is, we can launch two goroutines, both of these will write
+to this third channel. In the receiver, we will read from this single channel, as soon as any of the goroutines communicates.
+This has been implemented in this module.
+*/
+
+import (
+	"fmt"
+	"time"
+)
+
+func runFanIn() {
+	channel := fanIn()
+	for i := 0; i < 10; i++ {
+		fmt.Println(<-channel)
+	}
+}
+
+func generator(message string) <-chan string {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			channel <- fmt.Sprintf("%s: %s", "message: ", message)
+		}
+
+	}()
+
+	return channel
+}
+
+func fanIn() <-chan string {
+	channel := make(chan string)
+	channel1 := generator("first")
+	channel2 := generator("second")
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			channel <- <-channel1
+			time.Sleep(1000)
+		}
+	}()
+	go func() {
+		for i := 0; i < 5; i++ {
+			channel <- <-channel2
+		}
+	}()
+	return channel
+}
